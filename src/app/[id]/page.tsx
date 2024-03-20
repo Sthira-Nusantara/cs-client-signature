524cbb1bd9632f0a1560e9fb9d41cd1b641194e6
fauzanfebrian <fauzanfebrian1725@gmail.com>
2024-03-20 11:46:51 +0700

    signature layout us client

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,9 +1,7 @@
-import { Approver } from '@/types/signature'
-import moment from 'moment'
-import { FaCheckCircle } from 'react-icons/fa'
 import { getSignature } from './data'
 import InvalidSignature from './invalid-signature'
 import ServerError from './server-error'
+import SignatureLayout from './signature'
 
 export default async function Signature(props: { params: Record<string, string[] | string> }) {
     const data = await getSignature(props.params.id)
@@ -16,77 +14,5 @@ export default async function Signature(props: { params: Record<string, string[]
         return <ServerError />
     }
 
-    const approver = data.approvers.find(approver => approver.signature === props.params.id) as Approver
-
-    return (
-        <>
-            <div className="bg-white px-3 py-6 md:px-12 md:py-16 rounded" style={{ minHeight: '50%' }}>
-                <div>
-                    <FaCheckCircle className="text-green-600 text-8xl text-center mx-auto mb-2" />
-                    <hr />
-                </div>
-
-                <div className="my-3">
-                    <div className="flex justify-between flex-wrap gap-y-3">
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Status Dokumen</h5>
-                            <p>Aktif</p>
-                        </div>
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Nomor Dokumen</h5>
-                            <p>{data.usedBy}</p>
-                        </div>
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Nama Dokumen</h5>
-                            <p>{data.purpose}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="my-3">
-                    <h3 className="text-green-300">Info Penandatangan</h3>
-                    <hr className="mb-3" />
-                    <div className="flex justify-between flex-wrap gap-y-3">
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Nama</h5>
-                            <p>{approver.employee.name}</p>
-                        </div>
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">NIP</h5>
-                            <p>{approver.employee.nip}</p>
-                        </div>
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Jabatan</h5>
-                            <p>{approver.employee.position.name}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="my-3">
-                    <h3 className="text-green-300">Informasi</h3>
-                    <hr className="mb-3" />
-                    <div className="flex justify-between flex-wrap gap-y-3">
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Waktu Dibuat</h5>
-                            <p>{moment(data.createdAt).format('DD MMM YYYY HH:mm')}</p>
-                        </div>
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Waktu Pindai</h5>
-                            <p>{moment().format('DD MMM YYYY HH:mm')}</p>
-                        </div>
-                        <div className="md:w-1/3 w-full">
-                            <h5 className="font-bold text-base">Signature</h5>
-                            <p>{data.signature}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="mt-12">
-                <p className="text-lg text-white">
-                    Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang
-                    diterbitkan Rupira.
-                </p>
-            </div>
-        </>
-    )
+    return <SignatureLayout data={data} id={props.params.id as string} />
 }
---
+++ src/app/[id]/signature.tsx
@@ -0,0 +1,80 @@
+'use client'
+import { Approver, Signature } from '@/types/signature'
+import moment from 'moment'
+import { FaCheckCircle } from 'react-icons/fa'
+
+export default function SignatureLayout({ data, id }: { data: Signature; id: string }) {
+    const approver = data.approvers.find(approver => approver.signature === id) as Approver
+
+    return (
+        <>
+            <div className="bg-white px-3 py-6 md:px-12 md:py-16 rounded" style={{ minHeight: '50%' }}>
+                <div>
+                    <FaCheckCircle className="text-green-600 text-8xl text-center mx-auto mb-2" />
+                    <hr />
+                </div>
+
+                <div className="my-3">
+                    <div className="flex justify-between flex-wrap gap-y-3">
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Status Dokumen</h5>
+                            <p>Aktif</p>
+                        </div>
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Nomor Dokumen</h5>
+                            <p>{data.usedBy}</p>
+                        </div>
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Nama Dokumen</h5>
+                            <p>{data.purpose}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-3">
+                    <h3 className="text-green-300">Info Penandatangan</h3>
+                    <hr className="mb-3" />
+                    <div className="flex justify-between flex-wrap gap-y-3">
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Nama</h5>
+                            <p>{approver.employee.name}</p>
+                        </div>
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">NIP</h5>
+                            <p>{approver.employee.nip}</p>
+                        </div>
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Jabatan</h5>
+                            <p>{approver.employee.position.name}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-3">
+                    <h3 className="text-green-300">Informasi</h3>
+                    <hr className="mb-3" />
+                    <div className="flex justify-between flex-wrap gap-y-3">
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Waktu Dibuat</h5>
+                            <p>{moment(data.createdAt).format('DD MMM YYYY HH:mm')}</p>
+                        </div>
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Waktu Pindai</h5>
+                            <p>{moment().format('DD MMM YYYY HH:mm')}</p>
+                        </div>
+                        <div className="md:w-1/3 w-full">
+                            <h5 className="font-bold text-base">Signature</h5>
+                            <p>{data.signature}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-12">
+                <p className="text-lg text-white">
+                    Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang
+                    diterbitkan Rupira.
+                </p>
+            </div>
+        </>
+    )
+}
